5a99a942e343e50bab035e9af3571559fbafe596
Olaf Mihari <olafnjenga@gmail.com>
2023-04-28 04:16:35 +0300

    completed assignment

---
+++ src/components/MovieShow.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function MovieSHow( {movies} ){
+    // call useParams to access the `params` from the url
+    const params = useParams();
+    console.log(params);
+
+    return (
+        <div>
+            {
+                /*here we access the `movieId stored in `params` to render
+                information about the selected movie */
+            }
+            <h3>{movies[params.movieId].title}</h3>
+        </div>
+    );
+}
+
+export default MovieSHow;
